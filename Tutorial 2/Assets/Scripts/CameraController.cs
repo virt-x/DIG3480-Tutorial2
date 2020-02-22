80b5e536973058d4c7f12a3e3b8c7ab459ca973b
virt-x <xvirt@outlook.com>
2020-02-22 03:45:56 -0500

    tutorial 2 easy

---
+++ Tutorial 2/Assets/Scripts/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private Vector3 offset;
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+
+    void LateUpdate()
+    {
+        transform.position = new Vector3(player.transform.position.x - offset.x, /*player.transform.position.y -*/ offset.y, offset.z);
+    }
+}
---
+++ Tutorial 2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D body;
+    public float speed, jump;
+    public UnityEngine.UI.Text scoreText;
+    private int score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        body.AddForce(new Vector2(Input.GetAxis("Horizontal") * speed, 0));
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.CompareTag("Pickup"))
+        {
+            Destroy(collider.gameObject);
+            score++;
+            UpdateScore();
+        }
+    }
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Floor"))
+        {
+            if (Input.GetAxisRaw("Vertical") == 1 || Input.GetAxisRaw("Jump") == 1)
+            {
+                body.AddForce(new Vector2(0, jump), ForceMode2D.Impulse);
+            }
+        }
+    }
+
+    void UpdateScore()
+    {
+        scoreText.text = "Score: " + score;
+    }
+}
